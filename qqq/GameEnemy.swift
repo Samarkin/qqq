b5f74b639c87a18061fdad23df7e6d295b68b78b
Pavel Samarkin <samarkinpa@gmail.com>
2015-03-17 23:13:31 -0700

    Minor cleanup (no logic changes)

--- qqq/GameEnemy.swift
+++ qqq/GameEnemy.swift
@@ -1,11 +1,11 @@
 import SceneKit
 
 class GameEnemy {
-    private var node: SCNNode
+    private let node: SCNNode
     private unowned let game: GameController
     private var direction: GameDirection
     private var xy: (Int, Int)
-    private var stepTime: NSTimeInterval = 1
+    private let stepTime: NSTimeInterval = 1
     private var timeToMove = true
 
     init(onScene scene: GameScene, withController controller: GameController, atX x: Int, y: Int, facing dir: GameDirection, number: Int) {
@@ -30,7 +30,7 @@ class GameEnemy {
         if (timeToMove) {
             timeToMove = false
             let oldPosition = xy
-            var nextPosition = direction.getNextPosition(xy)
+            let nextPosition = direction.getNextPosition(xy)
             if game.itemAt(nextPosition).isPlayer {
                 return .GameOver
             }
@@ -60,31 +60,31 @@ class GameEnemy {
     }
 
     private class func createNode(#bodyColor: NSColor) -> SCNNode {
-        var bodyMaterial = SCNMaterial()
+        let bodyMaterial = SCNMaterial()
         bodyMaterial.diffuse.contents = bodyColor
         bodyMaterial.emission.contents = bodyColor
         bodyMaterial.emission.intensity = 1
-        var eyeMaterial = SCNMaterial()
+        let eyeMaterial = SCNMaterial()
         eyeMaterial.diffuse.contents = NSColor.whiteColor()
         eyeMaterial.emission.contents = NSColor.whiteColor()
         eyeMaterial.emission.intensity = 1
-        var light = SCNLight()
+        let light = SCNLight()
         light.type = SCNLightTypeOmni
         light.attenuationStartDistance = 5.0
         light.attenuationEndDistance = 15.0
         light.color = bodyColor
-        var node = SCNNode()
+        let node = SCNNode()
         node.geometry = SCNSphere(radius: 5)
         node.geometry!.materials = [bodyMaterial]
         node.light = light
 
-        var eye = SCNNode()
+        let eye = SCNNode()
         eye.geometry = SCNSphere(radius: 1)
         eye.geometry?.materials = [eyeMaterial]
         eye.position = SCNVector3(x: -1, y: 1.5, z: 4.2)
         node.addChildNode(eye)
 
-        var eye2 = SCNNode()
+        let eye2 = SCNNode()
         eye2.geometry = SCNSphere(radius: 1)
         eye2.geometry!.materials = [eyeMaterial]
         eye2.position = SCNVector3(x: 1, y: 1.5, z: 4.2)
